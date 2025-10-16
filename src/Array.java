d52d6e07a8abfd11f4f9eae8938ab13ea7f99b64
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2025-10-16 10:19:42 +0530

    2D array

--- src/Array.java
+++ src/Array.java
@@ -3,14 +3,19 @@ import java.util.*;
 public class Array {
     public static void main(String[] args) {
 
-        int[] nums = {1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(nums));
-        change(nums);
-        System.out.println(Arrays.toString(nums));
-    }
-        // mutability of arrays
-        static void change(int[] arr){
-               arr[0] = 4;
-        }
+      //2D arrays
+       // int[][] arr = new int[3][]; //specifying column size isn't really necessary
 
-}
+
+        //directly assigning values
+        int[][] arr = {  // here rows are 3 but column is different
+                {1,2,3},
+                {1,2},
+                {1,2,3,4}
+        };
+
+        // index
+        //if we do arr[0] //output -> 1 2 3
+        //if we do arr[0][0] //output -> 1
+
+}}
