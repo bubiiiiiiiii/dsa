331157d62eaa9561543a57d80378b9ec5be6ec9c
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2025-10-16 09:14:18 +0530

    array of objects

--- src/Array.java
+++ src/Array.java
@@ -1,6 +1,8 @@
 import java.util.*;
 public class Array {
     public static void main(String[] args){
+
+        //array of primitive
     int[] arr;  //declaration of  an array
         arr = new int[3];  //initialization of array  //here object is actually created in the memory
      Scanner sc = new Scanner(System.in);
@@ -17,4 +19,23 @@ public class Array {
         for(int i=0; i<num.length;i++) {
            num[i] = sc.nextInt();
         }
+
+        //enhanced for loop
+        for(int nums:num ){
+            System.out.println(nums);
+        }
+
+
+        //easier way to  print array
+        System.out.println(Arrays.toString(num)); //this method internally uses a for loop as well
+
+
+        //array of objects
+        String[] str = new String[5];
+
+        for(int i = 0 ; i<str.length ; i++ ){
+            str[i] = sc.next();
+        }
+        System.out.print(Arrays.toString(str));
+
 }}
