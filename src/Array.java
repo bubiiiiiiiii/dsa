622cc41b459fc8206ed478ca37e6569322871226
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2025-10-20 18:54:04 +0530

    basic operations on array

--- src/Array.java
+++ src/Array.java
@@ -1,38 +1,55 @@
-public class Array {
-
-
-    public static int findLargest(int[] arr) {
-
-        int max = arr[0];
-
-
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-        }
-
-        return max;
-    }
-
+public class Array{
     public static void main(String[] args) {
 
-        int[] arr1 = {3, 7, 2, 9, 1};
-        System.out.println("Array: [3, 7, 2, 9, 1]");
-        System.out.println("Largest: " + findLargest(arr1));
-        System.out.println();
+        int[] numbers = {10, 20, 30, 40, 50};
 
+        System.out.print("Original array: ");
+        printArray(numbers);
 
-        int[] arr2 = {-5, -2, -10, -1};
-        System.out.println("Array: [-5, -2, -10, -1]");
-        System.out.println("Largest: " + findLargest(arr2));
-        System.out.println();
+        // Access elements
+        System.out.println("First element: " + numbers[0]);
+        System.out.println("Last element: " + numbers[numbers.length - 1]);
 
+        // Modify an element
+        numbers[2] = 35;
+        System.out.print("After modifying third element: ");
+        printArray(numbers);
 
-        int[] arr3 = {100};
-        System.out.println("Array: [100]");
-        System.out.println("Largest: " + findLargest(arr3));
+        // Array length
+        System.out.println("Array length: " + numbers.length);
+
+        // Find sum
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+        System.out.println("Sum of all elements: " + sum);
+
+        // Loop through array
+        System.out.println("All elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Index " + i + ": " + numbers[i]);
+        }
+
+        // Find maximum value
+        int max = numbers[0];
+        for (int num : numbers) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        System.out.println("Maximum value: " + max);
+    }
+
+    // Helper method to print array
+    public static void printArray(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
     }
 }
-
-
