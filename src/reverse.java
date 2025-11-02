275a321f2aaa391fddaecaa2632e9e1b97f7c173
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2025-11-03 03:15:53 +0530

    reverse an array

--- src/reverse.java
+++ src/reverse.java
@@ -2,15 +2,22 @@ import java.util.Arrays;
 public class reverse {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
+       // swap(arr,0,4); //keep the index in mind  //don't need the swap statement in reverse question
+        reverse(arr);
         System.out.println(Arrays.toString(arr));
-        swap(arr,1,2);
+
     }
     static void reverse(int[] arr){
         int start = 0;
         int end = arr.length-1;
-    }
-    static void swap(int[] arr, int index1, int index2){
-        int temp = arr[index1];
-        arr[index1] = arr[index2];
+        while(start<end){  // forgot the while loop
+        swap(arr,start,end);  //index is start and end not 0 and 4
+        start++;
+        end--;
+    }}
+    static void swap(int[] arr, int index0, int index4){
+        int temp = arr[index0];
+        arr[index0] = arr[index4];
+        arr[index4] = temp;
     }
 }
