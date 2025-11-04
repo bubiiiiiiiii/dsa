850a25646251f7eb734fe076708c0fcc852735eb
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2025-11-05 00:14:22 +0530

    linear search(returning true or false)

--- src/linearSearch.java
+++ src/linearSearch.java
@@ -1,25 +1,22 @@
 public class linearSearch {
     public static void main(String[] args) {
         int[] arr = {3,54,23,46,76};
-        int target = 54;
-        int ans = linearsearch(arr, target);  //calling function
-        if(ans == -1) {
-            System.out.println("Element not found");
-        } else {
-            System.out.println(ans);
+        int target =54;
+        boolean ans = linearsearch(arr, target);  //calling function
+        System.out.println(ans);
         }
-    }
-    static int linearsearch(int[] arr, int target) {
+
+    static boolean linearsearch(int[] arr, int target) {
         if (arr.length == 0) {   //checking if array is empty
-            return -1;   //return nothing if array is  empty
+            return false;   //return nothing if array is  empty
         }
         for (int element : arr) {
             if (element == target) {
-                return element;  //returns element itself
+                return true;  //returns element itself
             }
 
 
         }
-        return -1;  //if target not found
+        return false;  //if target not found
 }
 }
