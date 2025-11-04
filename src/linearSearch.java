26079be60dcea23f4acf0924eb2c0bcf708c1837
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2025-11-05 00:10:32 +0530

    linear search(returning element instead of index)

--- src/linearSearch.java
+++ src/linearSearch.java
@@ -1,26 +1,25 @@
 public class linearSearch {
     public static void main(String[] args) {
         int[] arr = {3,54,23,46,76};
-        int target = 2;
+        int target = 54;
         int ans = linearsearch(arr, target);  //calling function
         if(ans == -1) {
             System.out.println("Element not found");
         } else {
-            System.out.println("Element found at index: " + ans);
+            System.out.println(ans);
         }
     }
     static int linearsearch(int[] arr, int target) {
         if (arr.length == 0) {   //checking if array is empty
             return -1;   //return nothing if array is  empty
         }
-
-        for (int index = 0; index < arr.length; index++) {  //iterating over every element to find target
-            int element = arr[index];   //naming element to compare with target
+        for (int element : arr) {
             if (element == target) {
-                return index;  //returns index of the target when found
+                return element;  //returns element itself
             }
+
+
         }
         return -1;  //if target not found
-    }
-
+}
 }
