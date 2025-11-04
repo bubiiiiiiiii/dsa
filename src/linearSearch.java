921965311de1ae18aaa3d91a7c38a370c1e3903d
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2025-11-05 00:30:34 +0530

    linear searching in string

--- src/linearSearch.java
+++ src/linearSearch.java
@@ -1,22 +1,18 @@
 public class linearSearch {
     public static void main(String[] args) {
-        int[] arr = {3,54,23,46,76};
-        int target =54;
-        boolean ans = linearsearch(arr, target);  //calling function
-        System.out.println(ans);
-        }
-
-    static boolean linearsearch(int[] arr, int target) {
-        if (arr.length == 0) {   //checking if array is empty
-            return false;   //return nothing if array is  empty
-        }
-        for (int element : arr) {
-            if (element == target) {
-                return true;  //returns element itself
-            }
-
-
-        }
-        return false;  //if target not found
+        String name = "bubi";
+        char target = 'u';
+        System.out.println(StringSearch(name, target));
 }
+   static boolean StringSearch(String name, char target){
+        if(name.length()==0){
+          return false;
+        }
+        for(int i=0; i<name.length(); i++){
+           if(target==name.charAt(i)){
+               return true;
+           }
+        }
+        return false;
+   }
 }
