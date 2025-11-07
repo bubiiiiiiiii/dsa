67a25e93841d9a0d94f4493944fcd23090e8fee6
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2025-11-07 23:27:32 +0530

    minimum number self written

--- src/MinimumNumber.java
+++ src/MinimumNumber.java
@@ -1,23 +1,17 @@
 public class MinimumNumber {
     public static void main(String[] args) {
         int[] arr = {12, 3, 19, -5, 7, 0};
-
-        int minValue = findMin(arr);
-        System.out.println("Minimum number in the array: " + minValue);
+        System.out.println(findMin(arr));
     }
 
-    static int findMin(int[] arr) {
-        if(arr == null || arr.length == 0) {
-            return Integer.MIN_VALUE;
-        }
+  static int findMin(int[] arr){
+       int ans = arr[0];
+       for(int i=1; i<arr.length; i++){
+           if(arr[i]<ans){
+               ans = arr[i];
+           }
+       }
+       return ans;
+  }
 
-        int min = arr[0];
-
-        for(int i = 1; i < arr.length; i++) {
-            if(arr[i] < min) {
-                min = arr[i];
-            }
-        }
-        return min;
-    }
 }
