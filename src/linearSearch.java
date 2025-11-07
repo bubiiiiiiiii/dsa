821f8edf99f35a2eb8e52b3a64f805aae19ea2bf
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2025-11-07 20:42:33 +0530

    searching in range using usual steps

--- src/linearSearch.java
+++ src/linearSearch.java
@@ -1,27 +1,22 @@
 public class linearSearch{
     public static void main(String[] args) {
-        int[] arr = {5, 12, 8, 20, 7, 15, 30};
-        int target = 7;
-        int start = 2;
-        int end = 5;
-
-        int result = searchInRange(arr, target, start, end);
-
-        if (result != -1) {
-            System.out.println("Element found at index: " + result);
-        } else {
-            System.out.println("Element not found in the given range.");
-        }
+      int[] arr = {3,56,34,32,67,23};
+      int target = 32;
+      int start = 1;
+      int end = 4;
+        System.out.println(searchInRange(arr, target, 1, 4));
     }
-
-    static int searchInRange(int[] arr, int target, int start, int end) {
-        if (arr == null || arr.length == 0) return -1;
-
-        for (int i = start; i <= end; i++) {
-            if (arr[i] == target) {
-                return i; // return index if found
+    static int searchInRange(int[] arr, int target, int start, int end){
+        if(arr.length == 0){
+            return -1;
+        }
+        for(int i = start; i<end; i++){
+            int element = arr[i];
+            if(element==target){
+                return i;
             }
         }
-        return -1; // not found
+        return -1;
+
     }
 }
