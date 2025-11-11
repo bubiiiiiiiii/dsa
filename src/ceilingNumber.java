93c35844d16ea4add09c8270339a5d37c3541e2f
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2025-11-11 20:41:25 +0530

    finding ceiling of a number(binary search q1)

---
+++ src/ceilingNumber.java
@@ -0,0 +1,30 @@
+// Q:If you’re given a sorted array and a target number,
+//the ceiling is the smallest element in the array that is greater than or equal to the target.
+//If no such element exists (i.e., all are smaller than target), we usually return -1 or indicate “no ceiling”.
+public class ceilingNumber {
+    public static void main(String[] args) {
+        int[] arr = {3,5,7,8,45,67,89,90};
+        int target = 80;
+        int ans  = ceilingBinary(arr, target);
+        System.out.println(ans);
+    }
+    static int ceilingBinary(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        if (target > arr[end]) return -1; // no ceiling exists
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return arr[mid];
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return arr[start];
+    }
+}
