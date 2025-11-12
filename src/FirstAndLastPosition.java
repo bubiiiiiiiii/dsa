de4be02dd54e4b8f96644d880c29b78caedf0e0b
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2025-11-12 21:55:44 +0530

    find the first and last position of the target in the array.(binary search)

---
+++ src/FirstAndLastPosition.java
@@ -0,0 +1,63 @@
+public class FirstAndLastPosition {
+
+
+    public static int findFirst(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int first = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                first = mid;
+                end = mid - 1;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return first;
+    }
+
+
+    public static int findLast(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int last = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                last = mid;
+                start = mid + 1;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return last;
+    }
+
+
+    public static int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return new int[]{first, last};
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+
+        int[] result = searchRange(nums, target);
+        System.out.println("First and Last Position: [" + result[0] + ", " + result[1] + "]");
+    }
+}
+
