fd6dc23737c219b3a3b1210aa06c0276bbe1d9b7
bubiiiiiiiii <dubeybhoomi43@gmail.com>
2025-11-22 23:48:15 +0530

    revisiting array

--- src/Array.java
+++ src/Array.java
@@ -1,1 +1,21 @@
-//overriding toString in objects
+//Given an integer array, find the second largest number in it without sorting the array.
+public class Array {
+    public static void main(String[] args) {
+        int[] arr = {12, 5, 8, 20, 3, 15};
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for(int num : arr) {
+            if(num > largest) {
+                secondLargest = largest;
+                largest = num;
+            }
+            else if(num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        System.out.println("Second Largest Number: " + secondLargest);
+    }
+}
